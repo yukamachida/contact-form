0f0afa2375b1795baadd2fb3370664fa7f7fa3ab
yuka machida <sh0ge0405@gmail.com>
2024-12-21 06:43:47 +0900

    git@github.com:yukamachida/contact-form.git

---
+++ src/resources/views/index.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contact Form</title>
+  <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}" />
+  <link rel="stylesheet" href="{{ asset('css/index.css') }}" />
+</head>
+
+<body>
+ <header class="header"> 
+  <div class="header__inner">
+    <a class="header__logo" href="/">
+      Contact Form  
+    </a>
+  </div>
+ </header>
+
+ <main>
+  <div class="contact-form__content">
+    <div class="contact-form__heading">
+      <h2>お問い合わせ</h2>
+  </div>
+  <form class="form">
+    <div class="form__group-title">
+
+</body>
+</html>
--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ContactController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/', [ContactController::class, 'index']);
+Route::post('/contacts/confirm', [ContactController::class, 'confirm']);
